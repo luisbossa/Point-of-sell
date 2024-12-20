8e3ecec2b739a60443795346b721317fbf4b4deb
luisbossa <luisbossa@gmail.com>
2024-12-20 17:04:01 -0600

    rer

--- app.js
+++ app.js
@@ -1,76 +1,62 @@
-// Importar los paquetes
 const express = require("express");
 const session = require("express-session");
 const path = require("path");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const methodOverride = require("method-override");
-const redis = require("redis");
-const connectRedis = require("connect-redis");
-
-// Cargar las variables de entorno
-dotenv.config();
 
 const app = express();
+dotenv.config();
 
-// Configuración de Redis para el almacenamiento de sesiones
-const RedisStore = connectRedis(session);
-const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST, // Dirección del servidor Redis
-  port: process.env.REDIS_PORT || 6379, // Puerto de Redis (predeterminado es 6379)
-  password: process.env.REDIS_PASSWORD, // Si Redis está protegido con contraseña
-});
+const expressLayout = require("express-ejs-layouts");
 
-redisClient.on("error", (err) => {
-  console.error("Error de conexión con Redis: ", err);
-});
+// Middlewares
 
-// Middleware de sesión utilizando RedisStore
+// Middleware de sesión
 app.use(
   session({
-    store: new RedisStore({ client: redisClient }),
-    secret: process.env.SESSION_SECRET, // Usa una clave secreta segura
-    resave: false, // No volver a guardar la sesión si no ha sido modificada
-    saveUninitialized: false, // No crear sesión si no hay datos
+    secret: process.env.JWT_SECRET, // Usa el secreto definido en .env
+    resave: false,
+    saveUninitialized: true,
     cookie: {
-      secure: process.env.NODE_ENV === "production", // Usa cookies seguras en producción (HTTPS)
-      httpOnly: true, // Impide que la cookie sea accesible desde JavaScript
-      maxAge: 1000 * 60 * 60 * 24, // Duración de la sesión: 1 día
+      secure: true, // Asegúrate de que la cookie sea segura en producción
+      httpOnly: true, // Asegura que solo el servidor pueda acceder a la cookie
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 1 semana
     },
   })
 );
 
-// Middleware para procesar datos de formularios
+// Para procesar datos enviados desde forms
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Middleware para cookies
+// For cookies
 app.use(cookieParser());
 
-// Configurar el motor de plantillas EJS
-const expressLayout = require("express-ejs-layouts");
+// Ejs layout
 app.use(expressLayout);
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// Servir archivos estáticos (por ejemplo, CSS, imágenes)
 app.use(express.static(path.join(__dirname, "public")));
 
-// Usar method-override para manejar el método _method (simulando PUT y DELETE)
+// Usa method-override para manejar el _method (simulando PUT)
 app.use(methodOverride("_method"));
 
 // Rutas
 app.use(require("./src/routes/router"));
 
-// Deshabilitar caché si no hay usuario autenticado
+// Clear cache
 app.use(function (req, res, next) {
   if (!req.user)
     res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
   next();
 });
 
-// Configuración del servidor
+// Server setup
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`Servidor en ejecución en https://localhost:${port}`);
 });
--- database/db.js
+++ database/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: process.env.MYSQL_HOST, // Utiliza la variable de entorno correcta
+  user: process.env.MYSQL_USER, // Utiliza la variable de entorno correcta
+  password: process.env.MYSQL_PASSWORD, // Utiliza la variable de entorno correcta
+  database: process.env.MYSQL_DATABASE, // Utiliza la variable de entorno correcta
+  port: process.env.MYSQL_PORT, // Asegúrate de usar el puerto correcto
 });
 
 connection.connect((error) => {
