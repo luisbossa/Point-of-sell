ad88047e38ed344f9c2a86eca81973d6d9c23487
luisbossa <luisbossa@gmail.com>
2024-12-20 23:05:41 -0600

    hhhhh

--- database/db.js
+++ database/db.js
@@ -1,18 +1,18 @@
-require('dotenv').config();  // Cargar las variables de entorno desde el archivo .env
+require('dotenv').config();  // Cargar variables de entorno
 
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,       // Usa la URL pública de la base de datos
-  port: process.env.DB_PORT,       // El puerto público es 3366
-  user: process.env.DB_USER,       // Tu usuario de la base de datos
-  password: process.env.DB_PASSWORD,  // Tu contraseña de la base de datos
-  database: process.env.DB_DATABASE  // El nombre de la base de datos
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 connection.connect((error) => {
   if (error) {
-    console.log("Error de conexión a la base de datos: " + error);
+    console.log("El error de conexión es: " + error);
     return;
   }
   console.log("Conectado a la base de datos MySQL!");
